dcb6815130ef14dae269f4246a49c7ad1ff2a627
Rodolfo Araújo <rodolfo@pop-os.localdomain>
2020-02-02 16:42:23 -0300

    Removing the tests from the solution class

--- src/main/scala/challenge_2_alphabet_position/AlphabetPosition.scala
+++ src/main/scala/challenge_2_alphabet_position/AlphabetPosition.scala
@@ -17,19 +17,5 @@ object AlphabetPosition extends App {
 
   def alphabetPosition(text: String): String =
     text.foldLeft("")(_ + getPosition(_)).tail
-
-  val test1 = "The sunset sets at twelve o' clock."
-  // should be 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11
-
-  println(test1)
-  println(alphabetPosition(test1))
-
-  println("\n")
-
-  val test2 = "The narwhal bacons at midnight."
-  // should be 20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20
-
-  println(test2)
-  println(alphabetPosition(test2))
 }
 
